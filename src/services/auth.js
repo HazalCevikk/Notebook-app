797e7a23fa8acffafa3e34d9436fc71bcd37fafd
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2023-02-07 21:51:13 +0300

    Add password hashing.

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,19 +1,23 @@
 import mongoClient from "@/services/mongo";
+import bcrypt from 'bcrypt'
+
 
 async function register({ username, password, firstName, lastName }) {
     const client = await mongoClient.connect()
     try {
         const db = client.db('auth')
         const collection = db.collection('users')
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
         const insertedDocument = await collection.insertOne({
-            username, password, firstName, lastName
+            username, password: hashedPassword, firstName, lastName
         })
         return insertedDocument
     } catch (e) {
 
         throw e
     }
-
 }
 
 export {
