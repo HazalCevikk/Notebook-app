b9364bc92960ce7570a39a5f729f9ce290887f20
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2023-02-09 06:31:00 +0300

    Add react settings to eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,18 @@ module.exports = {
     'import/extensions': ['off'],
     'import/no-unresolved': 'off',
     'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react/jsx-one-expression-per-line': ['off'],
+    'react/function-component-definition': ['off'],
+    'react/react-in-jsx-scope': ['off'],
+    'react/no-unstable-nested-components': ['off'],
+    'react/jsx-props-no-spreading': ['off'],
+    'react/button-has-type': ['off'],
+    'react/prop-types': ['off'],
+    'react/forbid-prop-types': ['off'],
+    'react/require-default-props': ['off'],
+    'react/jsx-indent': ['off'],
+    'react/no-unescaped-entities': ['off'],
+    'react/jsx-curly-newline': ['off'],
+
   },
 }
--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-function Login() {
-  return (
-    <>
-      <Head>
-        <title>Login page</title>
-      </Head>
-      <div>login page</div>
-    </>
-  );
-}
+const Login = () => (
+<>
+    <Head>
+      <title>Login page</title>
+    </Head>
+    <div>login page</div>
+</>
+)
 
 export default Login;
